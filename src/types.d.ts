9fec9110849f5f3f4fe3121c4896313875cba078
Blankeos <carloantonioct@gmail.com>
2022-10-06 15:38:48 +0800

    Extended types for parsed equation

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,3 +2,10 @@ interface InputType {
   z: string;
   constraints: string[];
 }
+
+interface IParsedEquation {
+  terms: string[];
+  coefficients: number[];
+  variables: string[];
+  rhs: number | null;
+}
